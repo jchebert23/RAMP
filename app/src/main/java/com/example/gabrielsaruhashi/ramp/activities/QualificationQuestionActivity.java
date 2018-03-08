f93c4cb85605f40d0ac4150048b0e2e6520a2297
gabrielsaruhashi <gabriel.saruhashi@gmail.com>
2018-03-08 17:16:08 -0500

    setting up tyler's branch

---
+++ app/src/main/java/com/example/gabrielsaruhashi/ramp/activities/QualificationQuestionActivity.java
@@ -0,0 +1,15 @@
+package com.example.gabrielsaruhashi.ramp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gabrielsaruhashi.ramp.R;
+
+public class QualificationQuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qualification_question);
+    }
+}
