1fc978557721b61b5510da4ff34df9fc91d6fc1f
Tyler Shen <tylershen0527@gmail.com>
2018-03-29 17:50:30 -0400

    qualification questions

--- app/src/main/java/com/example/gabrielsaruhashi/ramp/activities/SuggestionsActivity.java
+++ app/src/main/java/com/example/gabrielsaruhashi/ramp/activities/SuggestionsActivity.java
@@ -1,15 +1,18 @@
 package com.example.gabrielsaruhashi.ramp.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.gabrielsaruhashi.ramp.R;
 
-public class QualificationQuestionActivity extends AppCompatActivity {
+/**
+ * Created by tylershen on 3/29/18.
+ */
 
+public class SuggestionsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_qualification_question);
+        setContentView(R.layout.suggestions);
     }
 }
